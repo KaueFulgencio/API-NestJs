4dc6a2e2393706e38fa535eb727644c43438a03f
KaueFulgencio <Kauemfulgencio@outlook.com>
2023-12-28 11:03:02 -0300

    first commit
    API CRUD com TypeORM e MySQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,24 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
+import { TodosModule } from './todos/todos.module';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'kaue',
+      password: '',
+      database: 'api',
+      entities: [User], 
+      synchronize: true,
+    }),
+    TodosModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
